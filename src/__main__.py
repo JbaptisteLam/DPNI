70187f00fde42912b0b1ec380cf13b47469229a6
Jean-Baptiste Lamouche <jean-baptiste.lamouche@chru-strasbourg.fr>
2022-07-01 15:50:51 +0200

    store all analysis result even plot in files, then call generate_report func to generate html metrics with jinja template

--- src/__main__.py
+++ src/__main__.py
@@ -22,10 +22,15 @@ __version__ = "0.1"
 from estimation.seqff import Seqff
 from estimation.standard import Paternalidentification
 from estimation.hombased import Homozygotebased
-from utils.utils import fancystdout
+from utils.utils import fancystdout, fill_metrics, generate_report
 from parseargs.parseargs import parseargs
+from os.path import join as osj
 import sys
 import os
+import json
+import pandas as pd
+import plotly.utils as pu
+import plotly.io
 
 
 def main():
@@ -45,19 +50,34 @@ def main():
         os.mkdir(args.output)
     print(args)
     if args.func == "standard":
-        # 1) From CDC of DPNI study
+        # 1) From CDC of DPNI studyff
         pi = Paternalidentification(
             args.mum, args.dad, args.foetus, args.type, args.output, args.quality
         )
-        pi.main_paternal()
+        metrics_file = osj(args.output, "metrics.html")
+        metrics_csv = osj(args.output, "metrics.tsv")
+        paternal_var_rare, dico, ff, figjs = pi.main_paternal()
+        dico_val = {
+            "Method": [
+                "mean of allele frequency of heterozygous variants comming from father"
+            ],
+            "Variants count": [len(paternal_var_rare.index)],
+            "Foetal fraction": [ff],
+        }
+        df = fill_metrics(metrics_csv, dico_val)
+        # df.to_pickle(osj(args.output, "metrics.pickle"))
+        # generate_report(metrics_file, df, figjs)
 
     # 2) From combo_FF seqFF modele
     elif args.func == "seqff":
         print("#[INFO] FF estimation by seqff !")
-        if not "seqff" in args:
-            args.seqff = "estimation/seqff/pd4615-sup-0002-seqff.r"
+        if not args.seqff:
+            args.seqff = "/app/src/estimation/seqff/pd4615-sup-0002-seqff.r"
         pseq = Seqff(args.bfoetus, args.output, args.samtools, args.seqff)
-        pseq.analysis()
+        metrics_csv = osj(args.output, "metrics.tsv")
+        dico_val = pseq.analysis()
+        print(dico_val)
+        fill_metrics(metrics_csv, dico_val)
 
     # 3) From publication with UMI standard deviation
     elif args.func == "paternal":
@@ -71,9 +91,21 @@ def main():
             args.bedtools,
             args.rmasker,
         )
-        print(p)
-        p.get_ff()
-        p.get_ff_jiang()
+        ff_zhang, vafp, vafm = p.get_ff()
+        ff_jhiang, vars = p.get_ff_jiang()
+        out = "FF Zhang et al. :" + str(ff_zhang)
+        out2 = "FF Jiang et al. :" + str(ff_zhang)
+        metrics_file = osj(args.output, "metrics.html")
+        metrics_csv = osj(args.output, "metrics.tsv")
+        dico_val = {
+            "Method": [
+                "FF = 1 - mean(VAFm) + mean(VAFp)",
+                "FFi = 2p / p + q\n q as reference reads and p as alternate reads",
+            ],
+            "Variants count": [len(vafp.index) + len(vafm.index), len(vars.index)],
+            "Foetal fraction": [ff_zhang, ff_jhiang],
+        }
+        fill_metrics(metrics_csv, dico_val)
 
     # elif args.func == "plotvaf":
     #    scatter_vaf(args.dataframe, args.output, args.name)
@@ -81,6 +113,14 @@ def main():
     # getUMI("/home1/data/STARK/data/DPNI/trio/TWIST/FCL2104751.bwamem.bam", pos)
     # FF = VAFp + (1 - VAFm)
     # print("#[INFO] Estimation of Foetal fraction : ", FF)
+    elif args.func == "metrics":
+        with open(args.js) as json_file:
+            datatmp = json.load(json_file)
+            data = json.dumps(datatmp, cls=pu.PlotlyJSONEncoder)
+        df = pd.read_csv(args.df, sep="\t", header=0)
+        print(df)
+        print(type(plotly.io.from_json(data)))
+        generate_report(args.output, df, data)
 
 
 if __name__ == "__main__":
